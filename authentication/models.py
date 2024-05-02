d88f68ed198513687a7e6e28832e5a1c240c7de4
akzaylal <akshaylalsp6@gmail.com>
2024-05-02 20:47:47 +0530

    created user registration models

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class UserRegistration(AbstractUser):
+    CS = 'CS'
+    EC = 'EC'
+    EEE = 'EEE'
+    MECH = 'MECH'
+
+    DEPARTMENT_CHOICES = [
+        (CS, 'Computer Science'),
+        (EC, 'Electronics and Communication'),
+        (EEE, 'Electrical and Electronics'),
+        (MECH, 'Mechanical'),
+    ]
+
+    department = models.CharField(max_length=100, choices=DEPARTMENT_CHOICES)
+    registration_number = models.CharField(max_length=100)
+    admission_number = models.CharField(max_length=100)
+    cgpa = models.FloatField()
+    higher_secondary_score = models.CharField(max_length=100)
+    sslc_score = models.CharField(max_length=100)
+    mooc_course = models.CharField(max_length=100)
+    internship_attended = models.IntegerField(blank=True, null=True)
+    phone_number = models.CharField(max_length=15)
+    groups = models.ManyToManyField('auth.Group', related_name='custom_users_groups')
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_users_permissions')
